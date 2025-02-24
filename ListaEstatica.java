53ad030c711ba9e21cbf7e98ad0800051d2193a8
K3yg <eduardobrandt61@gmail.com>
2025-02-24 00:45:10 -0300

    init: init repo with static list

---
+++ ListaEstatica.java
@@ -0,0 +1,85 @@
+public class ListaEstatica extends IndexOutOfBoundsException {
+    private int[] info;
+    private int tamanho;
+
+    public int getTamanho() {
+        return tamanho;
+    }
+
+    public ListaEstatica() {
+        this.tamanho = 0;
+        this.info = new int[10];
+    }
+
+    public void redimensionar() {
+      int novaLista[] = new int[this.info.length + 10];
+      for (int i = 0; i < this.info.length; i++) {
+          novaLista[i] = this.info[i];
+      }
+      this.info = novaLista;
+    }
+
+    public void inserir(int valor) {
+      if (this.tamanho == this.info.length) {
+        redimensionar();
+      }
+      this.info[this.tamanho] = valor;
+      this.tamanho++;
+    }
+
+    public void exibir() {
+      for (int i = 0; i < this.tamanho; i++) {
+          System.out.print(this.info[i] + " ");
+      }
+    }
+
+    public String toString() {
+      String listaToString = "";
+      for (int i = 0; i < this.tamanho; i++) {
+          if (i == this.tamanho - 1) {
+              listaToString += this.info[i];
+          } else {
+              listaToString += this.info[i] + ", ";
+          }
+      }
+      return listaToString;
+    }
+
+    public int buscar(int valor) {
+      for (int i = 0; i < this.tamanho; i++) {
+          if (this.info[i] == valor) {
+              return i;
+          }
+      }
+      return -1;
+    }
+
+    public void retirar(int valor) {
+      int posicao = buscar(valor);
+      if (posicao != -1) {
+        for (int i = posicao; i < this.tamanho - 1; i++) {
+          // Move os elementos para a esquerda
+          //ex: [1, 2, 3, 4] retirar(2) => [1, 3, 4, 4]
+          this.info[i] = this.info[i + 1];
+        }
+        this.tamanho--;
+      }
+    }
+  
+    public void liberar() {
+      this.info = new int[10];
+      this.tamanho = 0;
+    }
+
+    public int obterElemento(int posicao) {
+      if (posicao >= 0 && posicao < this.tamanho) {
+        return this.info[posicao];
+      } else {
+        throw new IndexOutOfBoundsException("Posição inválida");
+      }
+    }
+
+    public boolean estaVazia() {
+      return this.tamanho == 0;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,42 @@
+public class Main {
+    public static void main(String[] args) {
+        ListaEstatica lista = new ListaEstatica();
+        for (int i = 1; i < 5; i++) {
+            lista.inserir(i*5);
+        }
+        System.out.print("Após a inserção dos valores, temos: ");
+        System.out.println(lista);
+
+        System.out.println("Tamanho da lista: " + lista.getTamanho());
+
+        System.out.println("Buscando o valor 15 na lista: " + lista.buscar(15));
+        System.out.println("Buscando o valor 30 na lista: " + lista.buscar(30));
+
+        lista.retirar(10);
+
+        System.out.print("Após a retirada do valor 10, temos: ");
+        System.out.println(lista);
+
+        System.out.println("Tamanho da lista: " + lista.getTamanho());
+
+        ListaEstatica lista2 = new ListaEstatica();
+        for (int i = 1; i < 16; i++) {
+            lista2.inserir(i);
+        }
+
+        System.out.print("Após a inserção dos valores, temos: ");
+        System.out.println(lista2);
+
+        System.out.println("Tamanho da lista: " + lista2.getTamanho());
+
+        lista2.liberar();
+
+        System.out.print("Após a liberação da lista, temos: ");
+        System.out.println(lista2 + "[vazia]");
+        System.out.println("Está vazia?  " + lista2.estaVazia());
+
+        System.out.println("Obtendo o valor na posição 3: " + lista.obterElemento(2));
+        System.out.println("Obtendo o valor na posição 5 (não existe): " + lista.obterElemento(5));
+
+    }
+}
